1ec5441ae6637593ae987e555d83b123b214f8fc
André Campanholo Paschoalini <paschoaliniandre@gmail.com>
2026-09-04 11:01:30 -0300

    init ex0.py

---
+++ Periodo_04/advanced_algorithms/exercicios/ex03.py
@@ -0,0 +1,11 @@
+def ex03():
+    N = int(intput())
+    for _ in range(N)
+        T_K = input().split()
+        T = T_K[0]
+        K = T_K[1]
+        duracoes = input().split()
+        dias = []
+        for i in range(K)
+            dias[i] = duracoes.remove()
+
